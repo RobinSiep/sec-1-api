dfc1e0ec49fe5507e202d9f91e703119af730dbb
rikvanderwerf <rikvanderwerf@outlook.com>
2016-10-04 13:20:18 +0200

    command endpoint

--- sec_1_api/handlers/index.py
+++ sec_1_api/handlers/index.py
@@ -11,3 +11,9 @@ log = logging.getLogger(__name__)
              request_method='GET')
 def root_view(request):
     return {"sec-1-api": "0.1"}
+
+
+@view_config(context=RootFactory, permission='public', renderer='json',
+             request_method='GET', name='command')
+def command(request):
+    return {"do": "vibrate"}
