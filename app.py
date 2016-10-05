af30a7189f11348cda2cd6a0a6a03f5d4563d8cd
rikvanderwerf <rikvanderwerf@outlook.com>
2016-10-05 10:10:02 +0200

    ja geen idee

--- app.py
+++ app.py
@@ -1,42 +1,15 @@
-from gevent import monkey
-monkey.patch_all()
-
-import cgi
-import redis
-from flask import Flask, render_template, request
-from flask_socketio import SocketIO
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-db = redis.StrictRedis('localhost', 6379, 0)
+app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 
-@app.route('/')
-def main():
-    return render_template('main.html')
-
-
-@app.route('/pymeetups/')
-def pymeetups():
-    return render_template('pymeetups.html')
-
-
-@socketio.on('connect', namespace='/dd')
-def ws_conn():
-    c = db.incr('connected')
-    socketio.emit('msg', {'count': c}, namespace='/dd')
-
-
-@socketio.on('disconnect', namespace='/dd')
-def ws_disconn():
-    c = db.decr('connected')
-    socketio.emit('msg', {'count': c}, namespace='/dd')
-
-@socketio.on('city', namespace='/dd')
-def ws_city(message):
-    print(message['city'])
-    socketio.emit('city', {'city': cgi.escape(message['city'])},
-                  namespace="/dd")
+@socketio.on('my_event', namespace='/')
+def test_message(message):
+    emit('my_response',
+         {'data': "test"})
 
 if __name__ == '__main__':
-    socketio.run(app, "0.0.0.0", port=5000)
+    socketio.run(app, "0.0.0.0", port=8000)
