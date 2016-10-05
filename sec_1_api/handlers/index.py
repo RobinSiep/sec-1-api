af5e952c68486fba51b356fb1bfeb2624012df17
rikvanderwerf <rikvanderwerf@outlook.com>
2016-10-05 11:06:20 +0200

    http update

--- sec_1_api/handlers/index.py
+++ sec_1_api/handlers/index.py
@@ -1,10 +1,14 @@
 import logging
 
 from pyramid.view import view_config
-
+from pyramid.response import FileResponse
 from sec_1_api.lib.factories.root import RootFactory
+import os
 
 log = logging.getLogger(__name__)
+
+
+here = os.path.dirname(os.path.abspath(__file__))
 
 
 @view_config(context=RootFactory, permission='public', renderer='json',
@@ -14,12 +18,15 @@ def root_view(request):
 
 
 @view_config(context=RootFactory, permission='public', renderer='json',
-             request_method='GET', name='command')
-def command(request):
+             request_method='GET', name='firmware')
+def firmware(request):
+    return {"version": "0.2"}
 
-    try:
-        identifier = request.headers['identifier']
-    except KeyError:
-        return {"error": "identifier not found"}
 
-    return {"do": "vibrate"}
+@view_config(context=RootFactory, permission='public',
+             request_method='GET', name='download')
+def firmware_download(request):
+    response = FileResponse(os.path.join(here, 'testfile.txt'), request=request)
+    response.headers['Content-Type'] = 'application/download'
+    response.headers['Content-Disposition'] = 'attachement; filename="firmware.txt"'
+    return response
