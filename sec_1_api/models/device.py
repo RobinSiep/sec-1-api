75633ffd9a4808bd6d155dd554044d0a0659ef9f
rik van der Werf <rikvanderwerf@outlook.com>
2016-10-12 16:24:39 +0200

    models

---
+++ sec_1_api/models/device.py
@@ -0,0 +1,31 @@
+import logging
+import uuid
+
+from sqlalchemy import Column, String
+from sqlalchemy_utils import UUIDType
+
+from sec_1_api.models.meta import Base, DBSession as session
+
+log = logging.getLogger(__name__)
+
+
+device_user = Table('device_user', Base.metadata,
+                    Column('link_id', UUIDType, ForeignKey('device.link_id')),
+                    Column('user_id', UUIDType, ForeignKey('user.id'))
+                    )
+
+
+class Device(Base):
+    __tablename__ = 'device'
+
+    link_id = Column(
+        UUIDType(binary=False), primary_key=True, default=uuid.uuid4)
+    secret_identifier = Column(String(250), unique=True)
+
+
+def get_device_by_link_id(link_id):
+    return session.query(Device).filter(Device.link_id == link_id).one()
+
+
+def get_device_by_secret_identifier(secret_identifier):
+    return session.query(Device).filter(Device.secret_identifier == secret_identifier).one()
---
+++ sec_1_api/models/firmware.py
@@ -0,0 +1,26 @@
+import datetime
+import logging
+import uuid
+
+
+from sqlalchemy import Column, String
+from sqlalchemy_utils import UUIDType
+
+from sec_1_api.models.meta import Base, DBSession as session
+
+log = logging.getLogger(__name__)
+
+
+class Firmware(Base):
+    __tablename__ = 'firmware'
+
+    id = Column(
+        UUIDType(binary=False), primary_key=True, default=uuid.uuid4)
+    firmware_version = Column(String(250), unique=True)
+    user_id = Column(UUIDType, ForeignKey('user.id'))
+    date_created = Column(
+        DateTime(timezone=True), default=datetime.datetime.utcnow())
+
+
+def get_latest_firmware():
+    return session.query(Firmware).order_by(ObjectRes.date_created.desc()).first()
