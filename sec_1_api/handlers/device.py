1d1e8a15e3d6c792b1e5f3159ea7782b6e8c14b1
Robin Siep <RobinSiep1@gmail.com>
2016-10-27 13:09:45 +0200

    unlink and extra validation

--- sec_1_api/handlers/device.py
+++ sec_1_api/handlers/device.py
@@ -3,11 +3,13 @@ import logging
 from marshmallow import ValidationError
 from pyramid.httpexceptions import HTTPBadRequest, HTTPInternalServerError
 from pyramid.view import view_config
+from sqlalchemy.orm.exc import NoResultFound
 
 from sec_1_api.lib.factories.root import RootFactory
-from sec_1_api.lib.validation.device import LinkDeviceSchema
+from sec_1_api.lib.validation.device import (LinkDeviceSchema,
+                                             UnlinkDeviceSchema)
 from sec_1_api.models import commit, persist, rollback
-from sec_1_api.models.device import (get_device_by_link_id,
+from sec_1_api.models.device import (Device, get_device_by_link_id,
                                      get_devices_by_user_id)
 
 log = logging.getLogger(__name__)
@@ -47,6 +49,36 @@ def link_device(request):
         persist(request.user)
     except:
         log.critical("Something went wrong saving the user",
+                     exc_info=True)
+        rollback()
+        raise HTTPInternalServerError
+    finally:
+        commit()
+
+
+@view_config(permission='device', context=RootFactory, name='device',
+             requrest_method='DELETE', renderer='json')
+def unlink_device(request):
+    try:
+        result, errors = UnlinkDeviceSchema(strict=True).load(
+            request.json_body)
+    except ValidationError as e:
+        raise HTTPBadRequest(json_body=e.messages)
+
+    user = request.user
+    try:
+        device = user.devices.filter(Device.name == result['name']).one()
+    except NoResultFound:
+        raise HTTPBadRequest(json_body={
+            'name': 'No device found for given name'
+        })
+
+    user.device.remove(device)
+
+    try:
+        persist(user)
+    except:
+        log.critical("Something went wrong unlinking the device",
                      exc_info=True)
         rollback()
         raise HTTPInternalServerError
