f4900f600f7b53a4879f19ce66fcaf7a4ea3b9c6
Robin Siep <RobinSiep1@gmail.com>
2016-11-03 16:27:04 +0000

    finishing touches

--- sec_1_api/websocket/SimpleExampleServer.py
+++ sec_1_api/websocket/SimpleExampleServer.py
@@ -1,4 +1,5 @@
 import asyncio
+import threading
 import glob
 import json
 import logging
@@ -12,6 +13,7 @@ from marshmallow import ValidationError
 from sqlalchemy.orm.exc import NoResultFound
 
 from sec_1_api.lib.validation.device import CommandSchema, FirmwareSchema
+from sec_1_api.models import expunge
 from sec_1_api.models.firmware import get_latest_firmware
 from sec_1_api.models.device import get_device_by_secret_identifier
 from sec_1_api.websocket.SimpleWebSocketServer import (WebSocket,
@@ -27,12 +29,25 @@ class SimpleEcho(WebSocket):
         super().__init__(*args, **kwargs)
         self.identifier = None
         self.files = None
-        self.pool = None
+        self.poll = False
+        self.pattern = None
+        self.on = None
+        self.stop = False
         self.root_dir = os.path.dirname(os.path.abspath(__file__))
-        log.info("woop")
         self.path = "/../sec_1_api/firmware/1.0.2"
-        log.info("boop")
+        thread = threading.Thread(target=self.poll_loop)
+        thread.start() 
+        log.info("init")
 
+    def poll_loop(self):
+        if self.stop:
+            return
+        log.info("poll")
+        log.info(self.poll)
+        if self.poll:
+             self.poll_for_command(self.identifier)
+        time.sleep(5)
+        self.poll_loop()
 
     def handleMessage(self):
         try:
@@ -41,9 +56,8 @@ class SimpleEcho(WebSocket):
             log.info(data)
             if data.get('identifier'):
                 log.info("identifying")
-                loop = asyncio.get_event_loop()
-                loop.run_until_complete(self.poll_for_command(data['identifier']))
-                loop.close()
+                self.identifier = data['identifier']
+                self.poll = True
                 return
             if data.get('result') is False:
                 log.info("restoring")
@@ -58,30 +72,30 @@ class SimpleEcho(WebSocket):
                 self.identifier = result['identifier']
                 firmware_version = result['firmware_version']
 
-            # latest_firmware_version = get_latest_firmware().firmware_version
-            log.info("comparins versions")
-					  # if float(firmware_version) < float(latest_firmware_version
-            log.info("updating versions")
-            self.path = '/../sec_1_api/firmware/1.0.2'
-            self.files = [filename for filename in glob.glob(
-            os.path.join('{}{}'.format(self.root_dir, self.path),
-                         '*.lua')
-            )]
+            latest_firmware_version = get_latest_firmware().firmware_version
+            log.info("comparing versions")
+            if float(firmware_version) < float(latest_firmware_version):
+                log.info("updating versions")
+                self.path = '/../sec_1_api/firmware/1.0.2'
+                self.files = [filename for filename in glob.glob(
+                os.path.join('{}{}'.format(self.root_dir, self.path),
+                             '*.lua')
+                )]
 
-            log.info("updating")
-				     # send the firmware
-            if not self.files:
-                log.info("update finish")
-                self.sendMessage("UPDATE FINISH")
-            filename = self.files[0]
-            del self.files[0]
+                log.info("updating")
 
-            with open(filename) as f:
-                content = f.readlines()
-            file_data = file_template.format(filename=filename.split('/')[-1],
-                                             content=content)
-            log.info("sending file")
-            self.sendMessage(str(file_data))
+                if not self.files:
+                    log.info("update finish")
+                    self.sendMessage("UPDATE FINISH")
+                filename = self.files[0]
+                del self.files[0]
+
+                with open(filename) as f:
+                    content = f.readlines()
+                file_data = file_template.format(filename=filename.split('/')[-1],
+                                                 content=content)
+                log.info("sending file")
+                self.sendMessage(str(file_data))
         except Exception as e:
             log.info(str(e))
 
@@ -90,21 +104,25 @@ class SimpleEcho(WebSocket):
         pass
 
     def handleClose(self):
-        if self.pool:
-            self.pool.shutdown(False)
+        self.stop = True
         log.info("closed")
 
 
     def poll_for_command(self, identifier):
-        log.info("called")
+        log.info("polling for command")
         try:
             device = get_device_by_secret_identifier(identifier)
-            self.sendMessage(str(CommandSchema().dump(device).data))
-            log.info("send {}".format(str(CommandSchema().dump(device).data)))
+            if device.on != self.on or device.pattern != self.pattern:
+                self.pattern = device.pattern
+                self.on = device.on
+                self.sendMessage(str(CommandSchema().dump(device).data))
+                log.info("send {}".format(str(CommandSchema().dump(device).data)))
+            else:
+                log.info("no change")
+            expunge(device)
         except NoResultFound:
             log.info("no device found")
             self.sendMessage(str({'message': 'device not found'})) 
-
 
 def serve(settings):
     log.info("init websocket")
